a5081b23ae9cf4c73c3dc8da81c568214073b4cb
linuxadvise <67831141+linuxadvise@users.noreply.github.com>
2022-03-05 17:47:11 +0530

    Create call_bash.py

---
+++ call_bash.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import subprocess
+def lambda_handler(event, context):   
+    print ("start")
+    subprocess.call("./generate_csv.sh")
+    print("end")
+    return {
+        'statusCode': 200,
+        'body': 'hello'
+    }
